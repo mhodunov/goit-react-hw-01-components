865ca9ea647ab4076af06a6fdef3a57d2db65805
Maksym Hodunov <m.godunov@yandex.ru>
2021-03-28 18:51:15 +0300

    feat: adds Profile component

---
+++ src/Profile/Profile.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import user from './user.json';
+
+const Profile = ({avatar, name, tag, location, followers, views, likes}) => (
+
+  <div className="profile">
+  <div className="description">
+    <img
+      src={avatar}
+      alt="Аватар пользователя"
+      className="avatar"
+    />
+    <p className="name">{name}</p>
+    <p className="tag">@{tag}</p>
+    <p className="location">{location}</p>
+  </div>
+
+  <ul className="stats">
+    <li>
+      <span className="label">Followers</span>
+      <span className="quantity">{followers}</span>
+    </li>
+    <li>
+      <span className="label">Views</span>
+      <span className="quantity">{views}</span>
+    </li>
+    <li>
+      <span className="label">Likes</span>
+      <span className="quantity">{likes}</span>
+    </li>
+  </ul>
+</div>
+
+)
+
+export default Profile;
