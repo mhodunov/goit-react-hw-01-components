7b2f417e304d90917a4b96fdb8103b6fd7e1b76c
Maksym Hodunov <m.godunov@yandex.ru>
2021-04-08 17:01:34 +0300

    styles: add styles for Profile component

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -2,33 +2,34 @@ import React from 'react';
 import user from './user.json';
 import defaultImage from './avatar.png';
 import PropTypes from 'prop-types';
+import styles from './Profile.module.css';
 
 const Profile = ({avatar, name, tag, location, followers, views, likes}) => (
 
-  <div className="profile">
-  <div className="description">
+  <div className={styles.profile}>
+  <div className={styles.description}>
     <img
       src={avatar}
       alt="Аватар пользователя"
-      className="avatar"
+      className={styles.avatar}
     />
-    <p className="name">{name}</p>
-    <p className="tag">@{tag}</p>
-    <p className="location">{location}</p>
+    <p className={styles.name}>{name}</p>
+    <p className={styles.tag}>@{tag}</p>
+    <p className={styles.location}>{location}</p>
   </div>
 
-  <ul className="stats">
+  <ul className={styles.stats}>
     <li>
-      <span className="label">Followers</span>
-      <span className="quantity">{followers}</span>
+      <span className={styles.label}>Followers</span>
+      <span className={styles.quantity}>{followers}</span>
     </li>
     <li>
-      <span className="label">Views</span>
-      <span className="quantity">{views}</span>
+      <span className={styles.label}>Views</span>
+      <span className={styles.quantity}>{views}</span>
     </li>
     <li>
-      <span className="label">Likes</span>
-      <span className="quantity">{likes}</span>
+      <span className={styles.label}>Likes</span>
+      <span className={styles.quantity}>{likes}</span>
     </li>
   </ul>
 </div>
